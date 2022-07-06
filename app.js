ca4c9e3aa3c5e89b4a3f43956aad5dbb14e044ec
DANIEL_FILIPE_SOARES_CORREA_VILACA <12102644@OMEGAB21.BARROCA>
2022-07-06 11:48:36 -0300

    Criação da estrutura da aplicação, e primeiros arquivos

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "views/public")));
+
+app.set('view engine', 'ejs');
+app.listen(port, ()=>{
+    console.log('Servidor rodando na porta ' + port);
+});
+
+app.get("/", (req, res)=>{
+    res.render('index', {});
+})
+
+app.get("/login", (req, res)=>{
+    res.render('curriculo', {});
+})
+
+
